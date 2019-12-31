6f61296a9bd2105305c2a4d8a7b2a9387e9c807b
Ali Waqar <aliwaqar981@gmail.com>
2019-12-31 19:22:01 +0500

    Updates

---
+++ precache-manifest.71156eaf9146249a03a3d8cba3ec8a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0cb58a4935a11c89db9a337fd1204a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88263668698083ad7928",
+    "url": "/monsters-rolodex/static/css/main.a07c4b6e.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88263668698083ad7928",
+    "url": "/monsters-rolodex/static/js/main.837e394c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
